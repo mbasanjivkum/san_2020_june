6d0ac95b53bedc4d78cefcffbf0c1efa008a4287
dell <dell@dell-PC>
2020-06-24 18:24:20 +0530

    okkk

--- src/main/java/p/c1.java
+++ src/main/java/p/c1.java
@@ -1,10 +1,14 @@
 package p;
 
-public class c1 {
+public class c1 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-System.out.println("hello");
+         System.out.println("hello");
+         
+         System.out.println("hiii");
 	}
 
 }
