ffbf44df9657a5f1e1b3ae26081cc2f45aa2b698
dell <dell@dell-PC>
2020-06-30 19:33:08 +0530

    okkk

--- src/test/java/cls1.java
+++ src/test/java/cls1.java
@@ -4,6 +4,8 @@ public class cls1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello");
+System.out.println("hiiii");
+System.out.println("fine");
 	}
 
 }
